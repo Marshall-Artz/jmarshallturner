0f1f26d275a815c4701e1e0499a11407dff72801
Marshall-Artz <marshallarts12@gmail.com>
2024-03-08 18:32:31 -0600

    Working router and navbar with refresh check

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectorRef, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @Input() tabPanel: any;
   links = ['About', 'Projects'];
-  activeLink = this.links[0]; // Default active set for 'About'
+  activeLink: string = '';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private changeDetector: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.updateActiveLink();
+      });
+  }
+
+  private updateActiveLink() {
+    const currentRoute = this.activatedRoute.firstChild?.snapshot.routeConfig?.path;
+    this.activeLink = currentRoute && this.links.includes(currentRoute) ? currentRoute : this.links[0];
+    this.changeDetector.detectChanges();
+  }
+
+  setActiveLink(link: string) {
+    this.activeLink = link;
+    this.changeDetector.detectChanges();
+  }
+
+  isActiveLink(link: string) {
+    return this.activeLink === link;
+  }
 }
